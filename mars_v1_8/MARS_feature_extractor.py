616f004ff549e1c6f216659bd8b5c14ce1cded9f
Ann Kennedy <ann.kennedy@northwestern.edu>
2022-04-25 13:57:59 -0500

    Update MARS_feature_extractor.py
    fix feature extraction when different classifiers have different requirements

--- mars_v1_8/MARS_feature_extractor.py
+++ mars_v1_8/MARS_feature_extractor.py
@@ -19,7 +19,9 @@ import scipy.io as sio
 import numpy.core.records as npc
 import joblib
 import yaml
+import copy
 import progressbar
+import pdb
 
 warnings.filterwarnings('ignore')
 sys.path.append('./')
@@ -3364,6 +3366,7 @@ def extract_features_wrapper(opts, video_fullpath, progress_bar_sig='', output_s
                                                   features=grps_to_add)
                     feat['features'] = feat_from_all_behaviors['features'] + feat['features']  # 'features' field reflects features in order added
                     feat['data_smooth'] = np.concatenate((feat_from_all_behaviors['data_smooth'], feat['data_smooth']), axis=2) if feat_from_all_behaviors['data_smooth'] else feat['data_smooth']
+                    feat_from_all_behaviors = copy.deepcopy(feat)
 
                 elif feature_type == 'raw_pcf':
                     num_mice = 2
